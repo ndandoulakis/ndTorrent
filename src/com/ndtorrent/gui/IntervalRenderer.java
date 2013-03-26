2dcf630a765e57d530f261b68b09304f48fa7af5
Nick Dandoulakis <ndandoulakis@hotmail.com>
2013-03-26 10:10:42 +0200

    display interval in weeks, days, hours, mins and secs

--- src/com/ndtorrent/gui/IntervalRenderer.java
+++ src/com/ndtorrent/gui/IntervalRenderer.java
@@ -17,10 +17,42 @@ public class IntervalRenderer extends DefaultTableCellRenderer {
 
 	private String getFormattedInterval(Long interval) {
 		long value = interval.longValue();
-		if (value >= 0)
-			return String.format("%ds", value);
-		else
+		if (value < 0)
 			return null;
-	}
 
+		// w d h m s
+		long w = value / (7 * 24 * 60 * 60);
+		value %= (7 * 24 * 60 * 60);
+		long d = value / (24 * 60 * 60);
+		value %= (24 * 60 * 60);
+		long h = value / (60 * 60);
+		value %= (60 * 60);
+		long m = value / (60);
+		long s = value % (60);
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(w);
+		builder.append('w');
+		builder.append(' ');
+		builder.append(d);
+		builder.append('d');
+		builder.append(' ');
+		builder.append(h);
+		builder.append('h');
+		builder.append(' ');
+		builder.append(m);
+		builder.append('m');
+		builder.append(' ');
+		builder.append(s);
+		builder.append('s');
+
+		// Examples
+		// 0w 0d 2h 0m 0s -> 2h 0m 0s
+		// 0s -> 0s
+		while (builder.length() > 2 && builder.charAt(0) == '0') {
+			builder.delete(0, 3);
+		}
+
+		return builder.toString();
+	}
 }
